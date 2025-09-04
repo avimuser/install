d0f91231db0ce5d8dc5b52ab4e1c0b1f57f7c45b
avimuser <174873767+avimuser@users.noreply.github.com>
2025-09-04 21:44:34 +0530

    Small fixes

--- arch-wsl/install.bash
+++ arch-wsl/install.bash
@@ -5,7 +5,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
 pacman -Syu --needed --noconfirm \
-    sudo man-db openssh git base-devel reflector \
+    sudo man-db openssh git base-devel reflector wl-clipboard \
     fzf fish stow wget neovim direnv git \
     gcc go npm
 
@@ -34,10 +34,14 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si && cd .. && rm -rf paru-bin
 paru -S antidot-bin ani-cli --skipreview --noconfirm
 antidot update
+cd ~
 rm .bash*
 '
 sed -i '$d' /etc/sudoers
 echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
+clear
+echo "Changing root password"
 passwd
+echo "Changing $USERNAME password"
 passwd $USERNAME
