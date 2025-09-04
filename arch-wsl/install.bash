d5ff99dc5aacb9e78ee7b8dd3f03eb2f53887751
avimuser <174873767+avimuser@users.noreply.github.com>
2025-09-04 18:24:16 +0530

    Initial commit

---
+++ arch-wsl/install.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+pacman -Syu --needed --noconfirm \
+    sudo man-db openssh git base-devel reflector \
+    fzf fish stow wget neovim direnv git \
+    gcc go npm
+
+CONFIG_MIRROR="https://raw.githubusercontent.com/avimuser/install/master"
+wget "$CONFIG_MIRROR/arch-wsl/reflector.conf" -O /etc/xdg/reflector/reflector.conf
+wget "$CONFIG_MIRROR/arch-wsl/reflector.timer" -O /usr/lib/systemd/system/reflector.timer
+systemctl enable reflector.timer
+
+ln -sf /bin/nvim /bin/vi
+ln -sf /bin/nvim /bin/vim
+
+USERNAME=avimuser
+useradd -mG wheel $USERNAME
+echo "[user]" >> /etc/wsl.conf
+echo "default=$USERNAME" >> /etc/wsl.conf
+
+chsh $USERNAME -s /bin/fish
+
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+su - $USERNAME -c '
+mkdir -p $HOME/Projects $HOME/.config
+git clone https://github.com/avimuser/dotfiles $HOME/Projects/dotfiles
+cd $HOME/Projects/dotfiles && stow config -t $HOME/.config
+
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin && makepkg -si && cd .. && rm -rf paru-bin
+paru -S antidot-bin ani-cli --skipreview --noconfirm
+antidot update
+rm .bash*
+'
+sed -i '$d' /etc/sudoers
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+passwd
+passwd $USERNAME
